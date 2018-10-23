21c11e948e403ffc5a4530ffbde9365ecbcc4b1a
Paweł Szybaj <pawelszybaj@wp.pl>
2018-10-23 12:07:40 +0200

    Clean up & finish

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
   // DB Params
   define("DB_HOST", 'localhost');
-  define("DB_USER", 'root');
-  define("DB_PASS", "home123456");
-  define("DB_NAME", "szmvc");
+  define("DB_USER", '_YOUR_USER');
+  define("DB_PASS", "_YOUR_PASS");
+  define("DB_NAME", "_YOUR_DB_NAME");
   // App Root
   define ("APPROOT",dirname(dirname(__FILE__)));
   // URL root
-  define("URLROOT", "http://localhost/szybajmvc");
+  define("URLROOT", "_YOUR_URL");
   // Site Name
-  define("SITENAME", "SzybajMVC");
+  define("SITENAME", "_YOUR_SITENAME");
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,13 @@
 <?php
   class Pages extends Controller {
     public function __construct() {
-      $this->postModel = $this->model("Post");
+    
     }
     public function index() {
-      $posts = $this->postModel->getPosts();
+      
       $data = 
         [
-          'title' => "WELCOME",
-          'posts' => $posts
+          'title' => "SzybajMVC",
         ];
         
       $this->view("pages/index", $data);
